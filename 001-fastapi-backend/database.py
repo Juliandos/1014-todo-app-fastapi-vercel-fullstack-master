ce669a7347e4cc24536b1be980b3d0183023eddb
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-10-09 10:04:20 -0500

    Fix: update dependencies for Python 3.13 compatibility

--- 001-fastapi-backend/database.py
+++ 001-fastapi-backend/database.py
@@ -6,18 +6,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ['DATABASE_USER']}:@{os.environ['DATABASE_HOST']}/{os.environ['DATABASE_NAME']}"
-
 user = os.environ['DATABASE_USER']
 password = os.environ['DATABASE_PASSWORD']
 host = os.environ['DATABASE_HOST']
 port = os.environ['DATABASE_PORT']
 db_name = os.environ['DATABASE_NAME']
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+# URL para psycopg2 (sincrónico)
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=10
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
